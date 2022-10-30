be99b3c5509503a872f105712e8d0f11b631dccb
sherkanII <111829943+sherkanII@users.noreply.github.com>
2022-10-29 19:18:18 -0500

    Add files via upload

---
+++ js/logic.js
@@ -0,0 +1,48 @@
+function openServic(){
+    document.getElementById("imgDog").style.display = 'none';
+    document.getElementById("frame1").style.display = 'block';
+    document.getElementById("frame2").style.display = 'none';
+    document.getElementById("frame3").style.display = 'none';
+    document.getElementById("frame4").style.display = 'none';
+    document.getElementById("contac").style.display = 'none';
+}
+function openGalery(){
+    document.getElementById("imgDog").style.display = 'none';
+    document.getElementById("frame1").style.display = 'none';
+    document.getElementById("frame2").style.display = 'block';
+    document.getElementById("frame3").style.display = 'none';
+    document.getElementById("frame4").style.display = 'none';
+    document.getElementById("contac").style.display = 'none';
+}
+function openNews(){
+    document.getElementById("imgDog").style.display = 'none';
+    document.getElementById("frame1").style.display = 'none';
+    document.getElementById("frame2").style.display = 'none';
+    document.getElementById("frame3").style.display = 'block';
+    document.getElementById("frame4").style.display = 'none';
+    document.getElementById("contac").style.display = 'none';
+}
+function openWe(){
+    document.getElementById("imgDog").style.display = 'none';
+    document.getElementById("frame1").style.display = 'none';
+    document.getElementById("frame2").style.display = 'none';
+    document.getElementById("frame3").style.display = 'none';
+    document.getElementById("frame4").style.display = 'block';
+    document.getElementById("contac").style.display = 'none';
+}
+function openContacts(){
+    document.getElementById("frame1").style.display = 'none';
+    document.getElementById("frame2").style.display = 'none';
+    document.getElementById("frame3").style.display = 'none';
+    document.getElementById("frame4").style.display = 'none';
+    document.getElementById("contac").style.display = 'block';
+}
+    function viewFacebook(){
+        window.location.href = "https://www.facebook.com/profile.php?id=100039212593273&sk=about";
+    }
+    function viewInstagram(){
+        window.location.href = "https://www.facebook.com/profile.php?id=100039212593273&sk=about";
+    }
+    function viewWhatsapp(){
+        window.location.href = "https://api.whatsapp.com/send?phone=573058177102&text=Hola%20My%20Pet";
+    }
