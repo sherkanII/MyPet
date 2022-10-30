5b0f5fadc0f1e7c4a5be32fde743a3c55883855f
sherkanmy <111829943+sherkanmy@users.noreply.github.com>
2022-10-30 15:01:58 -0500

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -41,7 +41,7 @@ function openContacts(){
         window.location.href = "https://www.facebook.com/profile.php?id=100039212593273&sk=about";
     }
     function viewInstagram(){
-        window.location.href = "https://www.facebook.com/profile.php?id=100039212593273&sk=about";
+        window.location.href = "https://www.instagram.com/my.pet2412/?hl=es-la";
     }
     function viewWhatsapp(){
         window.location.href = "https://api.whatsapp.com/send?phone=573058177102&text=Hola%20My%20Pet";
